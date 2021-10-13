0232218e4bce59f50db10ae5d005e1656c5b64cd
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2021-10-13 11:24:08 +0545

    Delete speech_recognizer.py

--- core/speech_recognizer.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-
-
-class SpeechRecognizer(sr.Recognizer, sr.):
-    def __enter__(self):
-        pass
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        pass
